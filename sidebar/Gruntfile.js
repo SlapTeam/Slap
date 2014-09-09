acbdd613766beeeb2cfb5eb54c6a1926067521c1
unknown <andre.rieussec@rightscale.com>
2014-09-09 15:11:31 -0700

    Removed GA, replaced compass with libsass

--- sidebar/Gruntfile.js
+++ sidebar/Gruntfile.js
@@ -44,9 +44,9 @@ module.exports = function (grunt) {
         files: ['test/spec/{,*/}*.js'],
         tasks: ['newer:jshint:test', 'karma']
       },
-      compass: {
+      sass: {
         files: ['<%= yeoman.app %>/styles/{,*/}*.{scss,sass}'],
-        tasks: ['compass:server', 'autoprefixer']
+        tasks: ['sass:server', 'autoprefixer']
       },
       gruntfile: {
         files: ['Gruntfile.js']
@@ -176,30 +176,26 @@ module.exports = function (grunt) {
     },
 
     // Compiles Sass to CSS and generates necessary files if requested
-    compass: {
+    sass: {
       options: {
-        sassDir: '<%= yeoman.app %>/styles',
-        cssDir: '.tmp/styles',
-        generatedImagesDir: '.tmp/images/generated',
-        imagesDir: '<%= yeoman.app %>/images',
-        javascriptsDir: '<%= yeoman.app %>/scripts',
-        fontsDir: '<%= yeoman.app %>/styles/fonts',
-        importPath: './bower_components',
-        httpImagesPath: '/images',
-        httpGeneratedImagesPath: '/images/generated',
-        httpFontsPath: '/styles/fonts',
-        relativeAssets: false,
-        assetCacheBuster: false,
-        raw: 'Sass::Script::Number.precision = 10\n'
+        // Making some
+        includePaths: ['./bower_components'],
       },
       dist: {
         options: {
-          generatedImagesDir: '<%= yeoman.dist %>/images/generated'
+          sourceComments: 'none'
+        },
+        files: {
+          '.tmp/styles/main.css' : '<%= yeoman.app %>/styles/main.scss'
         }
       },
       server: {
         options: {
-          debugInfo: true
+          sourceComments: 'map',
+          sourceMap: 'main.css.map'
+        },
+        files: {
+          '.tmp/styles/main.css' : '<%= yeoman.app %>/styles/main.scss'
         }
       }
     },
@@ -369,13 +365,13 @@ module.exports = function (grunt) {
     // Run some tasks in parallel to speed up the build process
     concurrent: {
       server: [
-        'compass:server'
+        'sass:server'
       ],
       test: [
-        'compass'
+        'sass'
       ],
       dist: [
-        'compass:dist',
+        'sass:dist',
         'imagemin',
         'svgmin'
       ]
