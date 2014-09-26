bd6db161452660003752445e5b0e39af753bf0f9
Barry Dorman <barrydormanjr@gmail.com>
2014-09-26 12:50:08 -0500

    Added a 'remove' link to slaps that you created

--- src/client/directives/menu/menu.js
+++ src/client/directives/menu/menu.js
@@ -4,29 +4,36 @@
 	slap.app.directive('slapMenu', ['$sce', function ($sce) {
     	return {
         	restrict: 'E',
-        	templateUrl: $sce.trustAsResourceUrl(chrome.extension.getURL('client/directives/menu/menu.html')),    
+        	templateUrl: $sce.trustAsResourceUrl(chrome.extension.getURL('client/directives/menu/menu.html')),
         	scope: {
             	availableSlaps: '=',
             	selectedSlap: '=',
             	open: '=',
                 onCreate: '='
             },
-        	link: function ($scope, element, attrs) { 
+        	link: function ($scope, element, attrs) {
+                $scope.slap = slap;
+
         		$scope.select = function(item) {
 					$scope.selectedSlap = item;
 				};
 
-                $scope.create = function() { 
+                $scope.create = function() {
                     if($scope.onCreate)
                         $scope.onCreate();
                 };
-                $scope.close = function() { 
+                $scope.close = function() {
                     $scope.open = false;
                     chrome.runtime.sendMessage({
                         type: "closemenu",
                         context: slap
                     });
                 };
+
+				$scope.remove = function(item) {
+				    $scope.availableSlaps.$remove(item);
+					$scope.availableSlaps.$save();
+				};
         	}
     	};
 	}]);
--- src/client/js/init.js
+++ src/client/js/init.js
@@ -7,5 +7,10 @@
 		'firebase.utils'
 	]);
 
+	// make slap object acessible by all scopes
+	slap.app.run(['$rootScope', function($rootScope) {
+		$rootScope.slap = slap;
+	}]);
+
 
 }(window.slap));
